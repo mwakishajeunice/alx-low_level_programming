fa3f52c89a8684d76b21922509778c46aec8db87
mwakishajeunice <mwakishajeunice@gmail.com>
2022-03-11 05:10:53 +0300

    Fixes the 4-puts.c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
-   *main - Prints a text
-   *
-   *Return: Always 0 (Success)
-   */
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
